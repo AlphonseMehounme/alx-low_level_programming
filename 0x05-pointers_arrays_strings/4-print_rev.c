7cebf216648f285b29919f82e2cc64860a23e455
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2022-12-19 18:32:37 -0800

    Reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio> 
+
+/**
+ * print_rev - print a string in reverse followed by a new line
+ * @s: char array string type
+ */
+
+void print_rev(char *s)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0'; i++)
+		;
+
+	for(j--; j >= 0; j--)
+		putchar(s[j]);
+
+	putchar('\n')
+}
