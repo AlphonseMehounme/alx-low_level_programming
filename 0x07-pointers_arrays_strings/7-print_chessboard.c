79593d96242c91544001df1f6a39eb10439dc398
Alphonse Mehounme <52801872+AlphonseMehounme@users.noreply.github.com>
2023-01-03 03:54:26 +0100

    Print chessboard update

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,4 +6,14 @@
  * Return: Nothing
  */
 
-void print_chessboard
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j;
+
+	for (; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
