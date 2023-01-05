dab90423e7fbcbe5c0137c4de657270c3395c03a
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2023-01-04 18:05:20 -0800

    Print the len of s

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string type input
+ * Return: the length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1))
+	return (0);
+}
