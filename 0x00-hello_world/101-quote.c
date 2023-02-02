2fc92c439df57f4b2253c88b87554906b7e8559d
Alphonse Mehounme <alphonsemehounme9@gmail.com>
2023-02-01 19:34:59 -0800

    Text to standard Error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 30);
 	return (1);
 }
