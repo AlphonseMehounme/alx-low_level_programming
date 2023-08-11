b2a086b13fa1d5a7922f6f36cb3bde6c9f8f89b1
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-08-11 14:26:33 -0700

    Betty compliance

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n < 0){
+	if (n < 0)
+	{
 		printf("%d is negative\n", n);
-	} else if(n == 0){
+	} else if (n == 0)
+	{
 		printf("%d is zero\n", n);
-	} else{
+	} else
+	{
 		printf("%d is positive\n", n);
 	}
 	return (0);
