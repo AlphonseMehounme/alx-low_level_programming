ec31c5f2c1c82202e2f197319ca42c0fd1565ba0
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-08-11 15:12:31 -0700

    Remove 1-last_digit and modify 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, lastd);
 	} else if (lastd < 6 && lastd != 0)
 	{
-		printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastd);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
