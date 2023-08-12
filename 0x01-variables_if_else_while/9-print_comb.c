671b97f188e2b99ff9433ff91681a18f3dd76699
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-08-12 06:24:09 -0700

    Print all possible combination of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
