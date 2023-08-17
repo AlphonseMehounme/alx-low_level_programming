2247e010bb8c54fcb99bc6c2126b2e4a6a2edecb
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-08-16 18:46:23 -0700

    Function that print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,39 @@
  */
 void times_table(void)
 {
-	int i, j, multi, mod;
+	int i, j, multi, mod, divm, multin;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; i++)
+		for (j = 0; j <= 9; j++)
 		{
-			printf(i*j);
+			multi = i * j;
+			multin = i * (j + 1);
+			if (multi < 10)
+			{
+				_putchar(48 + multi);
+				if (multin < 10 && j != 9)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				} else if (multin >= 10 && j != 9)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
+			} else
+			{
+				mod = multi % 10;
+				divm = multi / 10;
+				_putchar(48 + divm);
+				_putchar(48 + mod);
+				if (multin >= 10 && j != 9)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
+			}
 		}
 		_putchar('\n');
 	}
