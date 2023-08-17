2c8a1906531b2df7b424733f8a4fcc22f0eb62e2
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-08-16 19:58:38 -0700

    Fibo 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print the first 50 Fibonacci numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+	j = 1;
+	k = 2;
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d, ", j);
+		l = j + k;
+		j = k;
+		k = l;
+	}
+	print("\n");
+	return (0);
+
+}
