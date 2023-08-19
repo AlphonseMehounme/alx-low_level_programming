dc29d73c179c101ec8419012d33b92bba1c3a0a0
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-08-18 19:41:28 -0700

    Print integers

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -7,8 +7,21 @@
  */
 int main(void)
 {
-    print_square(2);
-    print_square(10);
-    print_square(0);
+	int m, n, o, p, q;
+    m = print_number(98);
+    printf("%d", m);
+    _putchar('\n');
+    n = print_number(402);
+    printf("%d", n)
+    _putchar('\n');
+    o = print_number(1024);
+    printf("%d", o);
+    _putchar('\n');
+    p = print_number(0);
+    printf("%d", p);
+    _putchar('\n');
+    q = print_number(-98);
+    printf("%d", q);
+    _putchar('\n');
     return (0);
 }
