50e474de6bed48f036bf1706396844cb1ee8b5c5
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-08-28 15:02:40 -0700

    Function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
