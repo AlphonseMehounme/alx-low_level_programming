8f06040bcc0be21f4c2a2e32fd43c0e8f5a8b374
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-08-29 16:24:40 -0700

    Function that copy src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy str to dest
+ * @src: String to copy
+ * @dest: Place to copy too
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	i++;
+	dest[i] = '\0';
+	return (dest);
+}
