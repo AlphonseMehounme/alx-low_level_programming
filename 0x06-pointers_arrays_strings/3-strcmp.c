6c8dbfe08e58b7c00acdecacae71326a72abccdf
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-07 15:56:17 -0700

    Add an extra condition to _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,9 +8,18 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n = 0;
-	int i;
+	int n, len1, len2;
+	int i, j, k;
 
+	n = len1 = len2 = 0;
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+		len1++;
+	}
+	for (k = 0; s2[k] != '\0'; k++)
+	{
+		len2++;
+	}
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] > s2[i])
@@ -21,6 +30,16 @@ int _strcmp(char *s1, char *s2)
 		{
 			n = -15;
 			break;
+		}
+	}
+	if (n == 0)
+	{
+		if (len1 > len2)
+		{
+			n = 15;
+		} else if (len1 < len2)
+		{
+			n = -15;
 		}
 	}
 	return (n);
