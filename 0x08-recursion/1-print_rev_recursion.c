4d7eabac1448eb6ec96f7267587c90d8cc030333
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-12 15:58:03 -0700

    Strlen recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: String to print in reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,2 @@
+/**
+ * _strlen_recursion - 
