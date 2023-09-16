fb2774833ace938c4c9b8c00cd6916a5ebf0cedb
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-16 10:43:59 -0700

    Replace my functions by stdio.h functions

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - Prints the number of args
@@ -10,7 +10,6 @@
 int main(int argc, char *argv[])
 {
 	(void) argv;
-	print_number(argc - 1);
-	_putchar('\n');
+	printf("%d\n", argc - 1);
 	return (0);
 }
