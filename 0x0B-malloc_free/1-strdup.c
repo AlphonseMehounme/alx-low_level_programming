47329d4836d7aa32a0dfe8b0971d843f8b6e8e1c
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-17 17:36:01 -0700

    Return the newly allocated memory with copy of str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - Return a function to a newly allocated memory with copy of str
+ * @str: Pointer to string to copy
+ * Return: Pointer to the new allocated memory
+ * NULL if str is NULL and NULL if not enough space allcated
+ */
+char *_strdup(char *str)
+{
+	char *p = NULL;
+	int i, size = 0;
+
+	if (str == NULL)
+	{
+		return (p);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size++;
+	}
+	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+	{
+		return (p);
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = str[i];
+	}
+	return (p);
+}
