4ad949db0fdf5c546e908c92ab76e878a08f206f
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-21 17:47:24 -0700

    Add definition condition on lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,10 +11,12 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+
+#endif
