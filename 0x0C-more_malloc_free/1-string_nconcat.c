7f91e5c3505113b56c0cddb0cc4653e7c5c5dbe3
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-24 07:06:28 -0700

    Add condition on s1 or s2 NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 	long unsigned int i, j, size = 1;
 
+	if (s1 == NULL)
+	{
+		s1[0] = '\0';
+	}
+	if (s2 == NULL)
+	{
+		s2[0] = '\0';
+	}
 	if (n >= strlen(s2))
 	{
 		n = strlen(s2);
