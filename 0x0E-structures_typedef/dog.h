bac04c63dfdbec1f1522ec41bb19a9aef0db4736
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-25 13:20:19 -0700

    Typedef and struct repo + first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Newtype for dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
