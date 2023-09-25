ca31e12827daad0ceaf63115fe7a47a4003cbd4a
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-24 18:23:31 -0700

    Handle NULL cases

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,41 +13,33 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	long unsigned int i, j, size = 1;
+	unsigned int i, j, size = 1, size1 = 0, size2 = 0;
 
 	if (s1 == NULL)
-	{
-		s1[0] = '\0';
-	}
+		size1 = 0;
+	else
+		size1 = strlen(s1);
 	if (s2 == NULL)
-	{
-		s2[0] = '\0';
-	}
-	if (n >= strlen(s2))
-	{
-		n = strlen(s2);
-	}
-	size += strlen(s1) + strlen(s2);
+		size2 = 0;
+	else
+		size2 = strlen(s2);
+	if (n >= size2)
+		n = size2;
+	size += size1 + size2;
 	s = malloc(size);
 	if (s == NULL)
-	{
 		return (s);
-	}
-	for (i = 0; i < strlen(s1); i++)
+	for (i = 0; i < size1; i++)
 	{
 		if (s1 == NULL)
-		{
 			break;
-		}
 		s[i] = s1[i];
 	}
 	j = 0;
-	for (; i < strlen(s1) + n; i++)
+	for (; i < size2 + n; i++)
 	{
 		if (s2 == NULL)
-		{
 			break;
-		}
 		s[i] = s2[j];
 		j++;
 	}
