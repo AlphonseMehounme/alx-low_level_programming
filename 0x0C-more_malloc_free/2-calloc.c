78c36090922a6bbb53f0d0135ed2b2569af4ff77
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-24 19:53:23 -0700

    Custom calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - Allocate memory for nmemb elements of size size
+ * @nmemb: Number of elements
+ * @size: Size of each element
+ *
+ * Return: Pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (p);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
