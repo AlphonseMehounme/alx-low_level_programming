1643968118707dff7883419a6934b22b618a4acd
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-25 19:58:25 -0700

    Add dynamic name2 and owner2 to get a copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,15 +13,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
+	char *name2, *owner2;
 
+	name2 = malloc(strlen(name) + 1);
+	owner2 = malloc(strlen(owner) + 1);
 	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	if (dog == NULL || name2 == NULL || owner2 == NULL)
 	{
+		free(name2);
+		free(owner2);
 		free(dog);
 		return (NULL);
 	}
-	dog->name = strdup(name);
+	strcpy(name2, name);
+	strcpy(owner2, owner);
+	dog->name = name2;
 	dog->age = age;
-	dog->owner = strdup(owner);
+	dog->owner = owner2;
 	return (dog);
 }
