370eb869a106d9c4b704a6271e5fd83113520aab
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-26 10:15:30 -0700

    Delete the first condition

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,13 +10,10 @@ int sum_listint(listint_t *head)
 {
 	int sum = 0;
 
-	if (head != NULL)
+	while (head != NULL)
 	{
-		while (head != NULL)
-		{
-			sum += head->n;
-			head = head->next;
-		}
+		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
