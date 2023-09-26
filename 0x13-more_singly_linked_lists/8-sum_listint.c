f14c2dcfe7c34402907d8b25bfb242d5f9efbb75
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-26 10:01:10 -0700

    Sum of data of list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - Sum of data of nodes
+ * @head: List
+ * Return: Sum of data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
