d86f6b38b2fdee622d2aefabd027e0bed4a04daa
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-26 12:45:10 -0700

    Update inser node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,33 +12,33 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newh = *head;
+	listint_t *oldh, *newh = *head;
 	listint_t *newnode;
 
 	if (newh == NULL)
 	{
 		return (NULL);
 	}
-	while (newh->next != NULL && idx > 1)
+	while (newh != NULL && idx > 0)
 	{
+		oldh = newh;
 		newh = newh->next;
 		idx--;
 	}
-	if (idx == 1 || idx == 0)
+	if (idx == 0)
 	{
 		newnode = malloc(sizeof(listint_t));
 		if (newnode == NULL)
 		{
 			return (NULL);
 		}
-		if (idx == 1)
+		if (idx == 0)
 		{
-			newnode->n = n;
-			newnode->next = newh->next;
-			newh->next = newnode;
+			newnode->next = newh;
+			oldh->next = newnode;
 			return (newnode);
 		}
-		if (idx == 0)
+		if (idx == 1)
 		{
 			newnode->n = n;
 			newnode->next = newh;
