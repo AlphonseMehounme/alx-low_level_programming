ce108167abc7ea79694d6480b65f205bf33cc211
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-26 17:14:36 -0700

    Replace newh by *head in attribution to newnode

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,33 +20,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->next = NULL;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
 	while (newh != NULL && count > 0)
 	{
 		oldh = newh;
 		newh = newh->next;
 		count--;
 	}
-	if (idx == 0 || count == 0)
+	if (count == 0)
 	{
-		newnode = malloc(sizeof(listint_t));
-		if (newnode == NULL)
-		{
-			return (NULL);
-		}
-		if (idx == 0)
-		{
-			newnode->n = n;
-			newnode->next = newh;
-			newh = newnode;
-			return (newnode);
-		}
-		if (count == 0)
-		{
-			newnode->n = n;
-			newnode->next = newh;
-			oldh->next = newnode;
-			return (newnode);
-		}
+		newnode->next = newh;
+		oldh->next = newnode;
+		return (newnode);
 	}
 	return (NULL);
 }
