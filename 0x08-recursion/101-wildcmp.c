3535c4849fd54aa4eb1f1e88f363891b15f3a1d4
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-27 05:08:00 -0700

    Modify return statement for *s2 is *

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,25 +8,20 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s2 == '\0')
 	{
-		return (1);
-	}
-	while (*s2 == '*' && *(s2 + 1) == '*')
-	{
-		s2++;
-	}
-	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) != '\0')
-	{
-		return (0);
-	}
-	if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
+		if (*s1 == '\0')
+			return (1);
+		else
+			return (0);
 	}
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	}
+	if (*s1 == *s2)
+	{
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
