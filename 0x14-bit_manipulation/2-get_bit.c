6178065e9d68bc19065efbedda22d4dc3e64dd1e
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-28 20:03:03 -0700

    Add description to the return value

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,6 +4,8 @@
  * get_bit - Get a bit at an index
  * @n: Number to get bit from
  * @index: Index
+ *
+ * Return: bit at index or -1 if failed
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
