7ed9603e608e1f2ffdbddec63757813fccd23543
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-29 08:34:16 -0700

    Number of bits to flip to get m from n

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - Return the n of bit to flip to get m
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: Number of bit to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int p, count = 0;
+
+	p = n ^ m;
+	while (p > 0)
+	{
+		if (p & 1)
+		{
+			count++;
+		}
+		p = p >> 1;
+	}
+	return (count);
+}
