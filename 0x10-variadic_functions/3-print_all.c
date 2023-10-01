48c43b590ed210c65d0912142e7f78b7488ee0aa
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-09-30 17:26:27 -0700

    Hnadle char arguments

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 void print_all(const char * const format, ...)
 {
+	va_list args;
+	char *speclist = "ceis";
+	int i = 0;
 
+	va_start(args, format);
+	while (format[i])
+	{
+		while(*speclist)
+		{
+			if (format[i] == *speclist)
+			{
+				printf("%c", va_arg(args, int));
+				break;
+			}
+			speclist++;
+		}
+		i++;
+		printf(", ");
+	}
+	printf("\n");
 }
