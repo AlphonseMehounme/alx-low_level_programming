a408d1a0bad066e055fed1b92ad833541ce45dc4
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-10-03 19:55:07 -0700

    Create file function

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - Create a new file
+ * @filename: Filename
+ * @text_content: Text content
+ *
+ * Return: 1 if successful or -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int cr, op, wr;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	cr = creat(filename, O_RDWR | O_CREAT | O_APPEND);
+	if (cr == -1)
+	{
+		return (-1);
+	}
+	op = open(filename, O_RDWR);
+	if (op == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		wr = write(op, text_content, strlen(text_content));
+	} else
+	{
+		wr = write(op, "", 1);
+	}
+	if (wr == -1)
+	{
+		close(op);
+		return (-1);
+	}
+	return (1);
+
+}
