2e45981c9db0e7f1e9e737fc43a83b195eee31cd
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-10-06 16:47:48 -0700

    Change the flow when filename is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,9 +30,6 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		wr = write(op, text_content, strlen(text_content));
-	} else
-	{
-		wr = write(op, "", 1);
 	}
 	if (wr == -1)
 	{
