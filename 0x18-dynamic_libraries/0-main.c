cb7d6e9bc0fa0c50961431eecf80c5173c8afeec
AlphonseMehounme <alphonsemehounme9@gmail.com>
2023-11-13 12:54:40 -0800

    libdynamic.so created

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -9,5 +10,5 @@
 int main(void)
 {
     printf("%d\n", _strlen("My Dyn Lib"));
-   return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
